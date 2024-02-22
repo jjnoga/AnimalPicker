b7d821518b527e9fe85081ade9412cf1c6b6e0d8
jjnoga <jjnoga@quinnipiac.edu>
2024-02-21 19:15:57 -0500

    added error message for invalid option

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -5,17 +5,19 @@ public class AnimalPicker {
         Scanner scan = new Scanner(System.in);
         int animal = 0;
 
-        while (animal <= 0 || animal >= 3) {
-            System.out.println("Choose an animal you would like to see: 1. Dog, 2. Cat");
-            animal = scan.nextInt();
-        }
+        
+        System.out.println("Choose an animal you would like to see: 1. Dog, 2. Cat");
+        animal = scan.nextInt();
 
         switch (animal) {
             case 2:
                 System.out.println("CAT ASCII ART HERE");
                 break;
-            default:
+            case 1:
                 System.out.println("DOG ASCII ART HERE");
+                break;
+            default:
+                System.out.println("Error! Selection not valid.");
         }
     }
 }
