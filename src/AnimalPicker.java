f9642b78d38a75548ef50797e0608ed4d364476d
jjnoga <jjnoga@quinnipiac.edu>
2024-02-21 19:13:14 -0500

    first branch: the initial commit

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class AnimalPicker {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scan = new Scanner(System.in);
+        int animal = 0;
+
+        while (animal <= 0 || animal >= 3) {
+            System.out.println("Choose an animal you would like to see: 1. Dog, 2. Cat");
+            animal = scan.nextInt();
+        }
+
+        switch (animal) {
+            case 2:
+                System.out.println("CAT ASCII ART HERE");
+                break;
+            default:
+                System.out.println("DOG ASCII ART HERE");
+        }
     }
 }
